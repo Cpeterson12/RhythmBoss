9a0b2ffd02d7380c5eae8054c21faf5e185a8df8
caden <cpete.est2014@gmail.com>
2023-04-04 19:08:01 -0600

    trying to add shake to numbers

--- Rhythm Game/Assets/scripts/ClickableBehaviour.cs
+++ Rhythm Game/Assets/scripts/ClickableBehaviour.cs
@@ -1,10 +1,12 @@
 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class ClickableBehaviour : OnBeatBehaviour
 {
-   public UnityEvent onTimeEvent, offBeatEvent;
+   public FloatData timerShake;
+   public UnityEvent onTimeEvent, offBeatEvent, offBeatDelayEvent;
    public void OnMouseDown()
    {
       if(onTime == true)
@@ -18,4 +20,22 @@ public class ClickableBehaviour : OnBeatBehaviour
       }
       
    }
+
+   public IEnumerator DelayBeat()
+   {
+      while (timerShake.value > 0)
+      {
+         Debug.Log("help");
+         yield return new WaitForSeconds(1.0f);
+         timerShake.value--;
+      }
+      offBeatDelayEvent.Invoke();
+   }
+
+   public void CallEvent()
+   {
+      StartCoroutine(DelayBeat());
+   }
 }
+
+
